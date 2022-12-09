9f8b228369bfa9e61109398f3eb5338d2c0df91d
drchey <derichey9@gmail.com>
2022-12-09 10:34:53 +0100

    First Commit

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express')
-const config = require('config')
+const dotenv = require('dotenv').config()
 const connectDB = require('./config/db')
-
+const cors = require('cors')
+const { errorHandler } = require('./middleware/error.middleware')
+const cookieParser = require('cookie-parser')
 const app = express()
 
 connectDB()
-const port = config.PORT || 5000
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+const port = process.env.PORT || 5000
+
+/** Routes  */
+
+app.use('/api/auth', require('./routes/auth.routes'))
+app.use('/api/posts', require('./routes/posts.routes'))
+app.use('/api/users', require('./routes/user.routes'))
+
+/** Call Error Handler */
+app.use(errorHandler)
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
